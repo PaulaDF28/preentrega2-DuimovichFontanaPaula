86b9adbb22ac5ee8d3429fd6fd934305da07a6ef
Paula Duimovich Fontana <105803147+PaulaDF28@users.noreply.github.com>
2023-09-18 20:15:51 -0300

    entrega fina

--- src/components/card/ButtonDetails.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-function ButtonDetails({item}) {
-  return(
-    <Link to={`/item/${item.id}`}>Ver detalles</Link>
-  );
-}
-
-export default ButtonDetails;
